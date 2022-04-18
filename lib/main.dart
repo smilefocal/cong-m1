97a5387265427517e8a47a0373b6391685f362f8
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-04-18 10:05:19 +0300

    refactor: add button material themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:congraph/styles/app_button_styles.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        elevatedButtonTheme: AppButtonStyles().elevatedButtonTheme,
+        textButtonTheme: AppButtonStyles().textButtonTheme,
+      ),
+    );
   }
 }
