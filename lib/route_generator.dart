b660d1f2662f137ffe2480061fc67c16988da68e
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-04-18 22:43:30 +0300

    feat:add the route generator

---
+++ lib/route_generator.dart
@@ -0,0 +1,21 @@
+import 'package:congraph/authentication/screen/authentication_page.dart';
+import 'package:congraph/home/screen/home_page.dart';
+import 'package:flutter/material.dart';
+
+///
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) => const AuthenticationPage(),
+        );
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      default:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+    }
+  }
+}
